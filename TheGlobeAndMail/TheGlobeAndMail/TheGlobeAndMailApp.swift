be31dcd2ca2b43fcb15994a8d7a25de33bad2b4b
Prasanth pc <prasanthpc@Prasanths-Mac-mini.local>
2024-10-23 11:39:51 -0400

    Initial project setup

---
+++ TheGlobeAndMail/TheGlobeAndMail/TheGlobeAndMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGlobeAndMailApp.swift
+//  TheGlobeAndMail
+//
+//  Created by Prasanth pc on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct TheGlobeAndMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
